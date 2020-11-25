16f9f8b257799835b7bd6f17e591cd89a0aaf06f
Bùi Trung Bảo <buitrbao222@gmail.com>
2020-11-26 00:39:57 +0700

    Update

--- Startup.cs
+++ Startup.cs
@@ -11,89 +11,79 @@ using Cinemo.Models;
 using Cinemo.Repository;
 using Cinemo.Service;
 
-namespace Cinemo
-{
-    public class Startup
-    {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddDbContext<ApplicationDbContext>(options =>
-              options.UseSqlite(
-                Configuration.GetConnectionString("DefaultConnection")));
-
-            services.AddDatabaseDeveloperPageExceptionFilter();
-
-            services.AddDefaultIdentity<User>()
-              .AddRoles<IdentityRole>()
-              .AddEntityFrameworkStores<ApplicationDbContext>();
-
-            services.AddRazorPages(options =>
-            {
-                options.Conventions.AuthorizeFolder("/Admin", "OnlyAdmin");
-            });
-
-            services.AddAuthorization(options =>
-            {
-                options.FallbackPolicy = new AuthorizationPolicyBuilder()
-                  .RequireAuthenticatedUser()
-                  .Build();
-                options.AddPolicy("OnlyAdmin", policy => policy.RequireRole("Admin"));
-            });
-
-            services.AddScoped<TheaterRepository>();
-            services.AddScoped<TheaterService>();
-
-            services.AddScoped<MovieRepository>();
-            services.AddScoped<MovieService>();
-
-            services.AddScoped<RoomRepository>();
-            services.AddScoped<RoomService>();
-
-            services.AddScoped<TicketTypeRepository>();
-            services.AddScoped<TicketTypeService>();
-
-            services.AddScoped<CategoryRepository>();
-            services.AddScoped<CategoryService>();
-
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseMigrationsEndPoint();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthentication();
-            app.UseAuthorization();
-
-            SeedData.Seed(userManager, roleManager);
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapRazorPages();
-            });
-        }
+namespace Cinemo {
+  public class Startup {
+    public Startup(IConfiguration configuration) {
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services) {
+      services.AddDbContext<ApplicationDbContext>(options =>
+        options.UseSqlite(
+          Configuration.GetConnectionString("DefaultConnection")));
+
+      services.AddDatabaseDeveloperPageExceptionFilter();
+
+      services.AddDefaultIdentity<User>()
+        .AddRoles<IdentityRole>()
+        .AddEntityFrameworkStores<ApplicationDbContext>();
+
+      services.AddRazorPages(options => {
+        options.Conventions.AuthorizeFolder("/Admin", "OnlyAdmin");
+      });
+
+      services.AddAuthorization(options => {
+        options.FallbackPolicy = new AuthorizationPolicyBuilder()
+          .RequireAuthenticatedUser()
+          .Build();
+        options.AddPolicy("OnlyAdmin", policy => policy.RequireRole("Admin"));
+      });
+
+      services.AddScoped<TheaterRepository>();
+      services.AddScoped<TheaterService>();
+
+      services.AddScoped<MovieRepository>();
+      services.AddScoped<MovieService>();
+
+      services.AddScoped<RoomRepository>();
+      services.AddScoped<RoomService>();
+
+      services.AddScoped<TicketTypeRepository>();
+      services.AddScoped<TicketTypeService>();
+
+      services.AddScoped<CategoryRepository>();
+      services.AddScoped<CategoryService>();
+
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env, UserManager<User> userManager, RoleManager<IdentityRole> roleManager) {
+      if (env.IsDevelopment()) {
+        app.UseDeveloperExceptionPage();
+        app.UseMigrationsEndPoint();
+      }
+      else {
+        app.UseExceptionHandler("/Error");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+      }
+
+      app.UseHttpsRedirection();
+      app.UseStaticFiles();
+
+      app.UseRouting();
+
+      app.UseAuthentication();
+      app.UseAuthorization();
+
+      SeedData.Seed(userManager, roleManager);
+
+      app.UseEndpoints(endpoints => {
+        endpoints.MapRazorPages();
+      });
+    }
+  }
 }
