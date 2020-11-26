2434d9f61581926858d827e48fd778d91a7399b5
Bùi Trung Bảo <buitrbao222@gmail.com>
2020-11-26 19:50:45 +0700

    Update Create show time

--- Models/Room.cs
+++ Models/Room.cs
@@ -1,30 +1,29 @@
 using System.ComponentModel.DataAnnotations;
 using System;
 using Cinemo.Interface;
-namespace Cinemo.Models
-{
-    public class Room : IEntity{
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public int NumCol { get; set; }
-        public int NumRow { get; set; }
-        public int Total { get; set; }
-        public string Formats { get; set; }
-        public enum Format {
-            _2D, 
-            _3D,
-            _IMAX
-        }
-    }
-public class RoomCreateDto {
+namespace Cinemo.Models {
+  public class Room : IEntity {
+    public int Id { get; set; }
     public string Name { get; set; }
-        public int NumCol { get; set; }
-        public int NumRow { get; set; }
-        public int Total { get; set; }
-        public string Formats { get; set; }
+    public int NumCol { get; set; }
+    public int NumRow { get; set; }
+    public int Total { get; set; }
+    public string Formats { get; set; }
+    public enum Format {
+      _2D,
+      _3D,
+      _IMAX
+    }
+  }
+  public class RoomCreateDto {
+    public string Name { get; set; }
+    public int NumCol { get; set; }
+    public int NumRow { get; set; }
+    public int Total { get; set; }
+    public string Formats { get; set; }
   }
 
-  public class RoomUpdateDto: RoomCreateDto {
+  public class RoomUpdateDto : RoomCreateDto {
     public int Id { get; set; }
   }
 }
--- Pages/Admin/Rooms/Create.cshtml.cs
+++ Pages/Admin/Rooms/Create.cshtml.cs
@@ -6,29 +6,28 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Cinemo.Service;
 using Cinemo.Models;
-namespace Cinemo.Pages.Admin.Room
-{
-    public class CreateModel : PageModel
-    {
-        private readonly RoomService service;
-        public CreateModel(RoomService service)
-        {
-        this.service = service;
-        }
-        [BindProperty]
-        public RoomCreateDto CreateDto { get; set; }
-
-        public string ErrorMessage {get; set;}
-        public IActionResult OnPost() {
-
-            if (service.GetDetail(CreateDto.Name)!=null) {
-                ErrorMessage = CreateDto.Name + " existed";
-
-                return Page();
-            }
-            service.Create(CreateDto);
-
-            return Redirect("/Admin/Rooms");
-        }
+namespace Cinemo.Pages.Admin.Room {
+  public class CreateModel : PageModel {
+    private readonly RoomService service;
+    public CreateModel(RoomService service) {
+      this.service = service;
     }
+
+    [BindProperty]
+    public RoomCreateDto CreateDto { get; set; }
+
+    public string ErrorMessage { get; set; }
+
+    public IActionResult OnPost() {
+
+      if (service.GetDetail(CreateDto.Name) != null) {
+        ErrorMessage = CreateDto.Name + " existed";
+
+        return Page();
+      }
+      service.Create(CreateDto);
+
+      return Redirect("/Admin/Rooms");
+    }
+  }
 }
