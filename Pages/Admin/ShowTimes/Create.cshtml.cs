e6aae0576fb80419fc88727ca513f26b8b989ee2
randy <randy@test-cube.com>
2020-12-04 09:23:00 +0000

    refactor create showtime

--- Pages/Admin/ShowTimes/Create.cshtml.cs
+++ Pages/Admin/ShowTimes/Create.cshtml.cs
@@ -9,63 +9,44 @@ using Cinemo.Models;
 using Microsoft.AspNetCore.Mvc.Rendering;
 namespace Cinemo.Pages.Admin.ShowTime
 {
-    public class CreateModel : PageModel
+  public class CreateModel : PageModel
+  {
+    private readonly ShowTimeService service;
+    private readonly MovieService movieService;
+    private readonly RoomService roomService;
+    public CreateModel(ShowTimeService service, TheaterService theaterService, MovieService movieService, RoomService roomService)
     {
-        private readonly ShowTimeService service;
-        private readonly MovieService movieService;
-        private readonly RoomService roomService;
-        public CreateModel(ShowTimeService service, TheaterService theaterService, MovieService movieService, RoomService roomService)
-        {
-            this.service = service;
-            this.movieService = movieService;
-            this.roomService = roomService;
-        }
-        [BindProperty]
-        public ShowTimeCreateDto CreateDto { get; set; }
-        public List<SelectListItem> movies { get; set; }
-        public List<SelectListItem> rooms { get; set; }
-        [BindProperty]
-        public string ErrorMessage { get; set; }
-        public List<SelectListItem> getMovies()
-        {
-            var movies = movieService.GetAll().Select(c => new SelectListItem
-            {
-                Value = c.Id.ToString(),
-                Text = c.Title
-            }).ToList();
-            return movies;
-        }
-        public List<SelectListItem> getRooms()
-        {
-            var rooms = roomService.GetAll().Select(c => new SelectListItem
-            {
-                Value = c.Id.ToString(),
-                Text = c.Theater.Name+"/ "+c.Name+"("+c.Formats+")"
-            }).ToList();
-            return rooms;
-        }
-
-        public void OnGet()
-        {
-            movies = getMovies();
-            rooms = getRooms();
-        }
-        public IActionResult OnPost()
-        {
-            try
-            {
-                service.Create(CreateDto);
-                return Redirect("./");
-                // return Page();
-            }
-            catch (Exception error)
-            {
-                ErrorMessage = error.Message;
-                movies = getMovies();
-                rooms = getRooms();
-                return Page();
-            }
-        }
+      this.service = service;
+      this.movieService = movieService;
+      this.roomService = roomService;
     }
+    [BindProperty]
+    public ShowTimeCreateDto CreateDto { get; set; }
+    public List<SelectListItem> MovieSelectList { get; set; }
+    public List<SelectListItem> RoomSelectList { get; set; }
+    [BindProperty]
+    public string ErrorMessage { get; set; }
+
+    public void OnGet()
+    {
+      MovieSelectList = movieService.GetSelectListItems();
+      RoomSelectList = roomService.GetSelectListItems();
+    }
+    public IActionResult OnPost()
+    {
+      try
+      {
+        service.Create(CreateDto);
+        return Redirect("./");
+      }
+      catch (Exception error)
+      {
+        ErrorMessage = error.Message;
+        MovieSelectList = movieService.GetSelectListItems(); ;
+        RoomSelectList = roomService.GetSelectListItems();
+        return Page();
+      }
+    }
+  }
 }
 
