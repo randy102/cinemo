ea1098253223d8f9b91369f6ac5ae7b2064262ef
randy <randy@test-cube.com>
2020-12-09 14:08:01 +0700

    Added Movie Data

--- Pages/Movie.cshtml.cs
+++ Pages/Movie.cshtml.cs
@@ -4,12 +4,35 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Cinemo.Service;
+using Cinemo.Models;
+namespace Cinemo.Pages
+{
+  public class MovieModel : PageModel
+  {
+    private readonly MovieService movieService;
+    private readonly TheaterService theaterService;
 
-namespace Cinemo.Pages {
-  public class MovieModel : PageModel {
     [BindProperty(SupportsGet = true)]
-    public string id { get; set; }
-    public void OnGet() {
+    public int id { get; set; }
+
+    public List<Theater> Theaters {get; set;}
+    public Cinemo.Models.Movie Movie {get; set;}
+
+    public MovieModel(MovieService movieService, TheaterService theaterService)
+    {
+      this.movieService = movieService;
+      this.theaterService = theaterService;
+    }
+
+    public void InitData(){
+      Movie = movieService.GetDetail(id);
+      Theaters = theaterService.GetAll();
+    }
+
+    public void OnGet()
+    {
+      InitData();
     }
   }
 }
