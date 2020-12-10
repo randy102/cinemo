177c1d025c13a94ce02e4892b61d1e91ea443d8b
randy <randy@test-cube.com>
2020-12-10 15:27:01 +0700

    Added user tickets

--- Models/User.cs
+++ Models/User.cs
@@ -1,7 +1,10 @@
 using Microsoft.AspNetCore.Identity;
+using System.Collections.Generic;
 
 namespace Cinemo.Models {
   public class User : IdentityUser {
     public string FullName { get; set; }
+
+    public virtual List<Ticket> Tickets {get; set;}
   }
 }
