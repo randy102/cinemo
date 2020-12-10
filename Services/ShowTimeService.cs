a2ceb29841f4e8cf83ee4314af23d6ad656d858b
Bùi Trung Bảo <buitrbao222@gmail.com>
2020-12-11 02:21:47 +0700

    Update book ticket

--- Services/ShowTimeService.cs
+++ Services/ShowTimeService.cs
@@ -8,60 +8,49 @@ using System;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Mvc.Rendering;
 
-namespace Cinemo.Service
-{
-  public class ShowTimeService
-  {
+namespace Cinemo.Service {
+  public class ShowTimeService {
     private ShowTimeRepository repository;
     private MovieRepository movieRepository;
     private RoomService roomService;
     private DateTimeUtils dateTimeUtils;
 
-    public ShowTimeService(MovieRepository movieRepository, ShowTimeRepository showTimeRepository, DateTimeUtils dateTimeUtils, RoomService roomService)
-    {
+    public ShowTimeService(MovieRepository movieRepository, ShowTimeRepository showTimeRepository, DateTimeUtils dateTimeUtils, RoomService roomService) {
       this.repository = showTimeRepository;
       this.roomService = roomService;
       this.movieRepository = movieRepository;
       this.dateTimeUtils = dateTimeUtils;
     }
 
-    public List<ShowTime> GetAll()
-    {
+    public List<ShowTime> GetAll() {
       return repository.FindAll();
     }
 
-    public List<ShowTime> GetAllPublished()
-    {
+    public List<ShowTime> GetAllPublished() {
       return repository.FindWhere(s => s.Status == ShowTime.ShowState.PUBLISHED);
     }
 
-    public ShowTime GetDetail(int id)
-    {
+    public ShowTime GetDetail(int id) {
       return repository.FindById(id);
     }
 
-    public List<ShowTime> GetByRoomId(int roomId)
-    {
+    public List<ShowTime> GetByRoomId(int roomId) {
       return repository.FindWhere(r => r.RoomId == roomId).ToList();
     }
 
-    public List<ShowTime> GetByMovie(Movie movie)
-    {
+    public List<ShowTime> GetByMovie(Movie movie) {
       return repository.FindWhere(r => r.MovieId == movie.Id).ToList();
     }
 
-    public List<SelectListItem> GetShowingSelectListItems(int defaultId = 0)
-    {
-      return GetShowingTime().Select(c => new SelectListItem
-      {
+    public List<SelectListItem> GetShowingSelectListItems(int defaultId = 0) {
+      return GetShowingTime().Select(c => new SelectListItem {
         Value = c.Id.ToString(),
         Text = c.Time.ToString(dateTimeUtils.FORMAT) + " - " + c.Movie.Title + " - " + c.Room.Name + " - " + c.Theater.Name,
         Selected = defaultId == c.Id
       }).ToList();
     }
 
-    public FilmStateEnum GetFilmStatus(ShowTime showTime)
-    {
+    public FilmStateEnum GetFilmStatus(ShowTime showTime) {
       DateTime start = showTime.Time;
       DateTime end = showTime.Time.AddMinutes(showTime.Movie.Length);
       DateTime now = DateTime.Now;
@@ -71,86 +60,75 @@ namespace Cinemo.Service
       return FilmStateEnum.PLAYING;
     }
 
-    public bool isNotEnd(ShowTime showTime)
-    {
+    public bool isNotEnd(ShowTime showTime) {
       DateTime start = showTime.Time;
       DateTime end = start.AddMinutes(showTime.Movie.Length);
       DateTime now = DateTime.Now;
       return now <= end;
     }
 
-    public List<ShowTime> GetShowingTime()
-    {
+    public List<ShowTime> GetShowingTime() {
       return GetAllPublished().Where(t => isNotEnd(t)).ToList();
     }
 
-    public List<ShowTime> GetShowingTimesByMovieId(int movieId)
-    {
+    public List<ShowTime> GetShowingTimesByMovieId(int movieId) {
       return GetShowingTime().Where(t => t.MovieId == movieId).ToList();
     }
 
-    public List<ShowTime> GetShowTimesByFilters(int movieId, int theaterId, string date)
-    {
+    public List<ShowTime> GetShowTimesByFilters(int movieId, int theaterId, string date) {
       var showTimes = GetShowingTimesByMovieId(movieId);
 
       if (theaterId != 0)
         showTimes = showTimes.Where(s => s.TheaterId == theaterId).ToList();
 
-      if (date != null)
-        showTimes = showTimes.Where(s =>
-        {
-          DateTime parsedDate = DateTime.ParseExact(date, "dd-MM-yyyy", null).Date;
-          return s.Time.Date == parsedDate;
-        }).ToList();
+      showTimes = showTimes.Where(s => {
+        DateTime parsedDate = date != null
+          ? DateTime.ParseExact(date, "dd-MM-yyyy", null).Date
+          : DateTime.Today.Date;
+
+        return s.Time.Date == parsedDate;
+      }).ToList();
 
       return showTimes;
     }
 
-    private void checkSupportedFormat(ShowTimeCreateDto dto)
-    {
+    private void checkSupportedFormat(ShowTimeCreateDto dto) {
       Room room = roomService.GetDetail(dto.RoomId);
-      if (!room.Formats.Contains(dto.Format.ToString()))
-      {
+      if (!room.Formats.Contains(dto.Format.ToString())) {
         throw new Exception("Not support the format " + dto.Format + ". Only " + room.Formats + ".");
       }
     }
 
 
-    private void checkDuplicated(ShowTimeCreateDto dto, int exceptId = 0)
-    {
+    private void checkDuplicated(ShowTimeCreateDto dto, int exceptId = 0) {
       Movie movie = movieRepository.FindById(dto.MovieId);
       List<ShowTime> showTimes = GetByRoomId(dto.RoomId);
       DateTime time = dateTimeUtils.Parse(dto.Time);
 
-      ShowTime dupplicated = showTimes.Where(s =>
-      {
+      ShowTime dupplicated = showTimes.Where(s => {
         var start = s.Time;
         var end = start.AddMinutes(s.Movie.Length);
         bool isBetween = start <= time && time <= end;
         return s.Id != exceptId && isBetween;
       }).FirstOrDefault();
 
-      if (dupplicated != null)
-      {
+      if (dupplicated != null) {
         DateTime dupEnd = dupplicated.Time.AddMinutes(dupplicated.Movie.Length);
         throw new Exception("Room is not available until " + dupEnd.ToString(dateTimeUtils.FORMAT));
       }
     }
 
-    public ShowTime Delete(int id)
-    {
+    public ShowTime Delete(int id) {
       ShowTime showTime = GetDetail(id);
       CheckShowtimeHasTicket(showTime);
       return repository.Delete(id);
     }
 
-    public ShowTime Create(ShowTimeCreateDto dto)
-    {
+    public ShowTime Create(ShowTimeCreateDto dto) {
       checkSupportedFormat(dto);
       checkDuplicated(dto);
 
-      var entity = new ShowTime
-      {
+      var entity = new ShowTime {
         RoomId = dto.RoomId,
         TheaterId = roomService.GetDetail(dto.RoomId).TheaterId,
         MovieId = dto.MovieId,
@@ -164,12 +142,10 @@ namespace Cinemo.Service
       return repository.Add(entity);
     }
 
-    public ShowTime Update(ShowTimeUpdateDto dto)
-    {
+    public ShowTime Update(ShowTimeUpdateDto dto) {
       checkSupportedFormat(dto);
       checkDuplicated(dto, dto.Id);
-      var entity = new ShowTime
-      {
+      var entity = new ShowTime {
         Id = dto.Id,
         RoomId = dto.RoomId,
         TheaterId = roomService.GetDetail(dto.RoomId).TheaterId,
@@ -183,13 +159,11 @@ namespace Cinemo.Service
       return repository.Update(entity);
     }
 
-    public void CheckShowtimeHasTicket(ShowTime showTime)
-    {
+    public void CheckShowtimeHasTicket(ShowTime showTime) {
       if (showTime.Tickets.Any()) throw new Exception("Showtime has been booked!");
     }
 
-    public ShowTime ChangeStatus(int id)
-    {
+    public ShowTime ChangeStatus(int id) {
       ShowTime showTime = GetDetail(id);
       CheckShowtimeHasTicket(showTime);
 
---
+++ wwwroot/js/book-ticket.js
@@ -0,0 +1,93 @@
+﻿$(function () {
+  // Tooltip props shared between proceed button tooltip and available tooltip
+  const tooltipSharedProps = {
+    delay: {
+      show: 100,
+      hide: 0,
+    }
+  }
+
+  let infoSeats = $('.info-content.info-seats');
+
+  // Initialize tooltip for proceed button
+  $('#proceed-btn-wrapper').tooltip({
+    ...tooltipSharedProps,
+    title: function () {
+      return "Please choose a ticket and a seat first.";
+    }
+  });
+
+  // Initialize tooltip for available seats
+  $('.seat-available').tooltip({
+    ...tooltipSharedProps,
+    title: function () {
+      return $(this).data('seat');
+    },
+  });
+
+  // Initialize choose ticket popover for available seats
+  $('.seat-available').popover({
+    html: true,
+    placement: 'top',
+    content: function () {
+      return $('#choose-ticket-popover').html();
+    },
+  });
+
+  // On choose ticket popover show
+  $('#choose-ticket-popover').on('show.bs.popover', function () {
+
+  });
+
+  const proceedBtn = $('#proceed-btn');
+
+  const proceedBtnWrapper = $('#proceed-btn-wrapper');
+
+  // Toggle chosen class on available seat click
+  $('.seat-available').click(function (e) {
+    $(this).toggleClass('seat-chosen seat-available');
+  });
+
+  // On available seat click
+  function onAvailableSeatClick() {
+    $(this).tooltip('hide').tooltip('disable');
+
+    //const thisSeat = $(this).data('seat');
+
+    //let chosenSeats = infoSeats.text().split(', ');
+
+    //chosenSeats = chosenSeats[0] === ""
+    //  ? [thisSeat]
+    //  : [...chosenSeats, thisSeat].sort((a, b) => a.localeCompare(b, 'en', { numeric: true }))
+
+    //infoSeats.text(chosenSeats.join(', '));
+
+    $(this).one('click', onChosenSeatClick);
+
+    //proceedBtn.prop('disabled', false)
+
+    //proceedBtnWrapper.tooltip('disable');
+  }
+
+  function onChosenSeatClick() {
+    $(this).tooltip('enable').tooltip('show');
+
+    const thisSeat = $(this).data('seat');
+
+    let chosenSeats = infoSeats.text().split(', ');
+
+    chosenSeats = chosenSeats.filter(seat => seat !== thisSeat);
+
+    infoSeats.text(chosenSeats.join(', '));
+
+    $(this).one('click', onAvailableSeatClick);
+
+    if (chosenSeats.length == 0) {
+      proceedBtn.prop("disabled", true)
+
+      proceedBtnWrapper.tooltip('enable');
+    }
+  }
+
+  $('.seat-available').one('click', onAvailableSeatClick);
+});
