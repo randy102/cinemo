4388dff7810d43414634849c1115de141a27834c
randy <randy@test-cube.com>
2020-12-10 11:52:46 +0700

    fix

--- Pages/BookTicket.cshtml.cs
+++ Pages/BookTicket.cshtml.cs
@@ -5,12 +5,14 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace Cinemo.Pages {
-  public class BookTicketModel : PageModel {
+namespace Cinemo.Pages
+{
+  public class BookTicket : PageModel
+  {
     [BindProperty(SupportsGet = true)]
     public int showtime { get; set; }
-
-    public void OnGet() {
+    public void OnGet()
+    {
     }
   }
 }
