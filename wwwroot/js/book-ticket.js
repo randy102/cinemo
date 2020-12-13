28e3d21b58fc689c9932766124652dc2e8989a16
Bùi Trung Bảo <buitrbao222@gmail.com>
2020-12-13 18:15:53 +0700

    Finish book ticket page

--- wwwroot/js/book-ticket.js
+++ wwwroot/js/book-ticket.js
@@ -1,5 +1,10 @@
 ﻿$(function () {
 
+  const showtimeId = new URLSearchParams(window.location.search).get('showtimeId');
+
+  let choosingSeat = "";
+
+  let chosenSeats = {};
 
   // Tooltip props shared between proceed button tooltip and available tooltip
   const tooltipSharedProps = {
@@ -8,8 +13,6 @@
       hide: 0,
     }
   }
-
-  let infoSeats = $('.info-content.info-seats');
 
   // Initialize tooltip for proceed button
   $('#proceed-btn-wrapper').tooltip({
@@ -30,87 +33,103 @@
     // Initialize popover for available seats
     .popover({
       html: true,
-      placement: function (popover, seat) {
-        return 'top';
-      },
+      placement: 'top',
       content: () => $('#choose-ticket-popover').html(),
       trigger: 'manual',
       sanitize: false,
     })
     // Available handler on mount
-    .on('click', onAvailableSeatClick);
+    .on('click', onAvailableSeatClick)
+    // Bind click functions to ticket row
+    .on('shown.bs.popover', function () {
+      $('.ticket-row').on('click', onTicketClick);
+    })
 
+  function updateSeats() {
+    $('#chosen-seats').html(
+      Object.keys(chosenSeats)
+        .sort()
+        .map(seat => `${seat} - ${chosenSeats[seat]}`)
+        .join('<br />')
+    );
+  }
 
   // On available seat click
   function onAvailableSeatClick() {
+    $('.seat-choosing')
+      .popover('hide')
+      .toggleClass('seat-choosing seat-available');
+
     $(this)
       .toggleClass('seat-available seat-choosing')
       .tooltip('hide')
       .tooltip('disable')
-      .off('click', onAvailableSeatClick)
+      .off('click')
       .on('click', onChoosingSeatClick)
       .popover('show')
 
-    // Popover automatically opens
-
-    //const thisSeat = $(this).data('seat');
-
-    //let chosenSeats = infoSeats.text().split(', ');
-
-    //chosenSeats = chosenSeats[0] === ""
-    //  ? [thisSeat]
-    //  : [...chosenSeats, thisSeat].sort((a, b) => a.localeCompare(b, 'en', { numeric: true }))
-
-    //infoSeats.text(chosenSeats.join(', '));
-
-    //proceedBtn.prop('disabled', false)
-
-    //proceedBtnWrapper.tooltip('disable');
-
-
+    choosingSeat = $(this).data('seat');
   }
 
+  // On choosing seat click (click seat again before choosing a ticket)
   function onChoosingSeatClick() {
     $(this)
       .toggleClass('seat-choosing seat-available')
-      .tooltip('enable')
-      .off('click', onChoosingSeatClick)
-      .on('click', onAvailableSeatClick)
       .popover('hide')
+      .tooltip('enable')
       .tooltip('show')
-
-    // Popover automatically closes
-
-
+      .off('click')
+      .on('click', onAvailableSeatClick)
   }
 
   // On chosen seat click
   function onChosenSeatClick() {
+    const thisSeat = $(this).data('seat');
+
     $(this)
       .toggleClass('seat-chosen seat-available')
-      .off('click', onChosenSeatClick)
+      .off('click')
       .on('click', onAvailableSeatClick);
 
+    // Remove the hidden input that has this seat
+    $('#ticket-for-seat-' + thisSeat).remove();
+
     // Remove this seat from chosenSeats array
-    let chosenSeats = infoSeats
-      .text()
-      .split(', ')
-      .filter(seat => seat !== $(this).data('seat'))
-      .join(', ');
+    delete chosenSeats[thisSeat];
+    updateSeats();
 
-    // Update seats info
-    infoSeats.text(chosenSeats);
+    // Disable proceed button
+    $('#proceed-btn').prop("disabled", true)
 
-    // If there are no seats left
-    if (chosenSeats.length == 0) {
-      // Disable proceed button
-      $('#proceed-btn').prop("disabled", true)
-
-      // Enable tooltip on proceed button
-      $('#proceed-btn-wrapper').tooltip('enable');
-    }
+    // Enable tooltip on proceed button
+    $('#proceed-btn-wrapper').tooltip('enable');
   }
 
+  function onTicketClick() {
+    const ticketId = $(this).data('ticket-id');
+    const ticketName = $(this).data('ticket-name');
 
+    // Update chosen seats in js
+    chosenSeats[choosingSeat] = ticketName;
+    updateSeats();
+
+    // Enable proceed button and disable it's tooltip
+    $('#proceed-btn').prop('disabled', false)
+    $('#proceed-btn-wrapper').tooltip('disable');
+
+    // Add hidden input to form
+    $('#ticket-hidden-input')
+      .clone()
+      .attr('id', 'ticket-for-seat-' + choosingSeat)
+      .val([showtimeId, choosingSeat, ticketId].join(','))
+      .appendTo('#proceed-btn-wrapper');
+
+    $('.seat-choosing')
+      .popover('hide')
+      .tooltip('enable')
+      .toggleClass('seat-choosing seat-chosen')
+      .off('click')
+      .on('click', onChosenSeatClick);
+  }
 
 });
