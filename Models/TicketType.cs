08aa61135cd5ff42b491e412cba22cc48151358c
randy <randy@test-cube.com>
2020-12-16 11:32:14 +0700

    check delete ticket type

--- Models/TicketType.cs
+++ Models/TicketType.cs
@@ -1,22 +1,31 @@
-
 using Cinemo.Interface;
-namespace Cinemo.Models {
-  public class TicketType : IEntity {
+using System.Collections.Generic;
+
+namespace Cinemo.Models
+{
+  public class TicketType : IEntity
+  {
     public int Id { get; set; }
     public Type Name { get; set; }
     public float Price { get; set; }
-    public enum Type {
+
+    public virtual List<Ticket> Tickets { get; set; }
+
+    public enum Type
+    {
       NORMAL,
       CHILDREN,
       STUDENT,
       VIP
     }
   }
-  public class TicketTypeCreateDto {
+  public class TicketTypeCreateDto
+  {
     public TicketType.Type Name { get; set; }
     public float Price { get; set; }
   }
-  public class TicketTypeUpdateDto : TicketTypeCreateDto {
+  public class TicketTypeUpdateDto : TicketTypeCreateDto
+  {
     public int Id { get; set; }
   }
 }
